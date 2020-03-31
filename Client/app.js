68009ee7e7f0743573ec216715d097aa88da0821
Charley <charleywright06@gmail.com>
2020-03-31 15:07:29 +0100

    Beginnings of client code, file structure

---
+++ Client/app.js
@@ -0,0 +1,12 @@
+function Submit() {
+	let placeholder = document.querySelector("input").placeholder;
+	switch (placeholder) {
+		case "Game PIN":
+			break;
+		case "Username":
+			break;
+		case "Delay":
+			break;
+	}
+	return false;
+}
