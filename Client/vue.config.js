9f6495484f42bdd4367f191c3f1c2abaa7608c12
Charley Wright <charleywright06@gmail.com>
2020-05-20 09:47:12 +0100

    PWA support

--- Client/vue.config.js
+++ Client/vue.config.js
@@ -1,3 +1,24 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  pwa: {
+    name: "Kahoot Rocks",
+    themeColor: "#3f51b5",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "#121212",
+    manifestOptions: {
+      name: "Kahoot Rocks",
+      short_name: "Kahoot.rocks",
+      start_url: ".",
+      display: "standalone",
+      themeColor: "#3f51b5"
+    },
+    iconPaths: {
+      favicon32: "img/icons/favicon-32x32.png",
+      favicon16: "img/icons/favicon-16x16.png",
+      appleTouchIcon: "img/icons/apple-touch-icon-152x152.png",
+      maskIcon: "img/icons/safari-pinned-tab.svg",
+      msTileImage: "img/icons/msapplication-icon-144x144.png"
+    }
+  }
 };
---
+++ Server/public/precache-manifest.d51e1acbcb9282c4b655d3c72a601de1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5df97d9c82d3cd83541ca62196e833b",
+    "url": "/css/chunk-vendors.69c1cea7.css"
+  },
+  {
+    "revision": "075fd7a840cc735dabf2",
+    "url": "/css/chunk-vendors.a54a5dfb.css"
+  },
+  {
+    "revision": "6a059c94e957f28b21aae75f2a957ce7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55518088e0351613e46685131c52d5a4",
+    "url": "/js/app.34f17eaf.js"
+  },
+  {
+    "revision": "d22b3efc5793a1ed583c",
+    "url": "/js/app.9559587a.js"
+  },
+  {
+    "revision": "985b40510f72d48948d6d81d911526f2",
+    "url": "/js/app.ac746474.js"
+  },
+  {
+    "revision": "075fd7a840cc735dabf2",
+    "url": "/js/chunk-vendors.13f81e86.js"
+  },
+  {
+    "revision": "3aa661e0c5a414a5d2ada671da3d6966",
+    "url": "/js/chunk-vendors.4c40b246.js"
+  },
+  {
+    "revision": "c626d4131f9f5f5ecd4aafae4ba83149",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "653d1d2984ef9332f664b6659794e632",
+    "url": "/privacy.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "684a8fce2110afa20e9e384901e9df30",
+    "url": "/style.css"
+  },
+  {
+    "revision": "8ba208b0180d409cb8a8e443ad9709f6",
+    "url": "/terms.html"
+  }
+]);
