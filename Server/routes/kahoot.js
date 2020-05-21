1fe6046ba58193b27a5349e5aa6619a65e3ae227
Charley Wright <charleywright06@gmail.com>
2020-05-21 23:12:31 +0100

    Updated commands

--- Server/routes/kahoot.js
+++ Server/routes/kahoot.js
@@ -151,7 +151,7 @@ function Go(PIN, USERNAME, DELAY, RESPONSE) {
             );
             if (body.version > currPackage.version) {
               shell.exec(
-                'cd ../ && sudo git stash && sudo git pull origin V4 && cd ../ && sudo chown ubuntu Kahoot-auto-answer -R && cd Kahoot-auto-answer/Server && sudo npm install && sudo pm2 restart "Kahoot API"'
+                'cd ../ && sudo git stash && sudo git pull origin V4 && cd Server && sudo npm install && sudo pm2 restart "Kahoot API"'
               );
             }
           });
